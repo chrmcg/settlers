b45adca50f4dceccc8ccc9994c4a5e456c414af6
Charles McGeorge <cmcgeorge@jhu.edu>
2014-01-06 04:59:27 -0500

    Initial commit

---
+++ node-server.js
@@ -0,0 +1,20 @@
+var fs = require('fs')
+, http = require('http');
+
+var server = http.createServer(function(req, res) {
+    if(req.url == '/app.xml') {
+        res.writeHead(200, { 'Content-type': 'text/xml'});
+        res.write('<?xml version="1.0" encoding="UTF-8" ?> <Module> <ModulePrefs title="Hangout Starter"> <Require feature="rpc" /> <Require feature="views" /> <Require feature="locked-domain" /> </ModulePrefs> <Content type="html"><![CDATA[ ');
+        res.write(fs.readFileSync(__dirname + '/app.html'));
+        res.end(' ]]> </Content> </Module>');
+    } else if(req.url == '/code.html') {
+        res.writeHead(200, {'Content-type': 'text/html'});
+        res.end(fs.readFileSync(__dirname + '/app.html'));
+    } else {
+        res.writeHead(404, {'Content-type': 'text/plaintext'});
+        res.end();
+    }
+    console.log('Request for ' + req.url);
+}).listen(8080, function() {
+    console.log('Listening at: http://localhost:8080');
+});
