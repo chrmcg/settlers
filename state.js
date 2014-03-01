3ff4d5dff832de304a9978452a35029732293a2a
Marc Reisner <reisner.marc@gmail.com>
2014-03-01 18:05:53 -0500

    attempt at having game be reloadable

--- state.js
+++ state.js
@@ -248,12 +248,10 @@ game.state.download = function(state) {
         }
     }
 
-    if(window.drawn === true) {
-        game.board.redraw();
-    }
     var num = this.getLocalPlayerNumber();
     // This makes sure that this doesn't happen before everything is initialized
-    if (game.startbox.wrapper_outer === undefined) {
+    if (window.drawn === true) {
+        game.board.redraw();
         if(this.turn === num) {
             if((this.next_action === 'rollDice' && window.diceRolled === false) || ((this.phase === 0 || this.phase === 1) && this.next_action === 'buildSettlement')) {
                 game.actions.cancelSelect();
