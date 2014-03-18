69a33e4342e085be8216bf32f5adb2695fbea353
Charles McGeorge <cmcgeorge@jhu.edu>
2014-03-18 12:37:49 -0400

    Added offline_demo.html

--- node-server.js
+++ node-server.js
@@ -30,6 +30,12 @@ var server = http.createServer(function(req, res) {
             res.write(fs.readFileSync(__dirname + '/' + filename));
         });
         res.write(fs.readFileSync(__dirname + '/app_2.html'));
+    } else if(req.url == '/offline-scripts') {
+        res.writeHead(200, {'Content-type': 'text/javascript'});
+        source_files.forEach(function(filename) {
+            res.write(fs.readFileSync(__dirname + '/' + filename));
+        });
+        res.end();
     } else {
         res.writeHead(404, {'Content-type': 'text/plaintext'});
         res.end();
